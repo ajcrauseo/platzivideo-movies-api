2f4446c2fc1a5c0024c4ffb820845c247cafed79
Antony Cabeza <antonycabeza@gmail.com>
2021-07-25 18:16:15 -0400

    [Create] Agregando la coleccion de user-movies

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies.js');
+const userMoviesApi = require('./routes/userMovies');
 const {
   logErrors,
   errorHandler,
@@ -15,9 +16,10 @@ app.use(express.json());
 
 // routes
 moviesApi(app);
+userMoviesApi(app);
 
 // Catch 404 error
-app.use(notFoundHandler)
+app.use(notFoundHandler);
 
 // Errors middleware
 app.use(logErrors);
