22bd0c11c025b7ac5d4b4ae81e0087fcf7591892
Antony Cabeza <antonycabeza@gmail.com>
2021-08-09 11:36:52 -0400

    [Update] Añadiendo configuracion de Helmet

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 
 const { config } = require('./config/index');
@@ -14,6 +15,8 @@ const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 // body parser
 app.use(express.json());
+
+app.use(helmet());
 
 // routes
 authApi(app);
